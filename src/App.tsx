7e2b42ea31a0aae4b247138e8fe449d7d4be6c4e
Adrien Treuille <adrien.g.treuille@gmail.com>
2024-12-08 20:15:31 -0800

    Starting to hack the SolidJS hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
@@ -13,6 +13,24 @@ const App: Component = () => {
         </p>
         <p>
           This is the beginning of a <i>new</i> app by Adrien Treuille
+        </p>
+        <p>
+          <svg height="300" width="400">
+            <defs>
+              <linearGradient id="gr1" x1="0%" y1="60%" x2="100%" y2="0%">
+                <stop
+                  offset="5%"
+                  style="stop-color:rgb(255,255,3);stop-opacity:1"
+                />
+                <stop
+                  offset="100%"
+                  style="stop-color:rgb(255,0,0);stop-opacity:1"
+                />
+              </linearGradient>
+            </defs>
+            <ellipse cx="125" cy="150" rx="100" ry="60" fill="url(#gr1)" />
+            Sorry but this browser does not support inline SVG.
+          </svg>
         </p>
         <a
           class={styles.link}
